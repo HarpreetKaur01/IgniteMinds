69db9fa05163219d38b856a7b4199e8bc72fc7df
Preet Kaur <harpreet@gmail.com>
2025-09-17 23:08:39 -0400

    video fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -93,7 +93,7 @@ function Home() {
         <section style={{backgroundColor: "#F5F5F5"}} className ="custom-container py-5 video-section ">
         <h2>Watch Our Introduction</h2>
       <div className="video-container">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/AG3Pzq9EAWo?si=Q7nYGfkFzJBG0Srs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe src="https://www.youtube.com/embed/AG3Pzq9EAWo?si=Q7nYGfkFzJBG0Srs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
        
       </div>
         <p>
